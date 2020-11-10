9511a99c2cbda1ab927430c62d5e97673fa341db
tiiedye <tiiedye@gmail.com>
2020-11-10 09:31:36 -0800

    adding source files

---
+++ SimpleCalculator/C#/MyCalculatorv1/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace MyCalculatorv1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private int Test(int x) 
+        { 
+            int y = 0; 
+            if (x == 0) 
+            { 
+                y = 1; 
+            } 
+            else if (x == 1) 
+            { 
+                y = 2; 
+            }
+            else
+            {
+                y = 3;
+            }
+            return y; 
+        }
+    }
+}
